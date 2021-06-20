498f2323d4165b6741f91a91a2e7883218b0bce1
ZockerBoys HD <ZockerBoysHD@outlook.de>
2021-06-20 11:48:54 +0200

    fixxes ??

--- client.lua
+++ client.lua
@@ -9,11 +9,11 @@
 
 local markerType = 1 -- Change to a Marker of your choice - https://docs.fivem.net/docs/game-references/markers/
 
-local markerColorR = 255 -- Change Marker color here (RED)
-local markerColorG = 255 -- Change Marker color here (GREEN)
+local markerColorR = 10 -- Change Marker color here (RED)
+local markerColorG = 255 -- Change Magithrker color here (GREEN)
 local markerColorB = 255 -- Change Marker color here (BLUE)
 
-local afterBurn = 2000 -- How long should the Marker be drawn after the range has been changed?
+local afterBurn = 5000 -- How long should the Marker be drawn after the range has been changed?
 
 local isDrawing = false -- don't touch!
 local curProx = 0.0 -- don't touch!
@@ -21,24 +21,31 @@ local curProx = 0.0 -- don't touch!
 RegisterNetEvent('SaltyChat_VoiceRangeChanged')
 AddEventHandler('SaltyChat_VoiceRangeChanged', function(range)
 
+
+   
     isDrawing = true
-    curProx = range
+    curProx = tonumber(range)
 
-    drawMarker()
-
+    CreateThread(function() 
+     drawMarker()
+    end)
     Wait(afterBurn)
 
+   
     isDrawing = false
 
 end)
 
 function drawMarker()
 
+    
     while isDrawing do
 
-        local playerCoords = GetEntityCoords(PlayerPedId())
+      
+        local posPlayer = GetEntityCoords(PlayerPedId())
 
-        DrawMarker(1, playerCoords.x, playerCoords.y, playerCoords.z - 1, 0, 0, 0, 0, 0, 0, curProx * 2, curProx * 2, 1, 0, markerColorR, markerColorG, markerColorB, 0, 0, 0, 0)
+     
+        DrawMarker(markerType, posPlayer.x, posPlayer.y, posPlayer.z - 1, 0, 0, 0, 0, 0, 0, curProx * 2, curProx * 2, 0.8001, 0, markerColorR, markerColorG, markerColorB, 0, 0, 0, 0)
 
         Wait(1)
 
